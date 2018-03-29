f59a44c40d679d5316c4e5f4e624c5f23aa80013
mckasap <mckasap@gmail.com>
2018-03-29 11:23:09 +0300

    Toolbar oluşturma Click e cevap henuz yok

--- app/src/main/java/org/kasapbasi/a2018week0702/MainActivity.java
+++ app/src/main/java/org/kasapbasi/a2018week0702/MainActivity.java
@@ -2,6 +2,8 @@ package org.kasapbasi.a2018week0702;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mi= getMenuInflater();
+        mi.inflate(R.menu.mymenu, menu);
+        return true;
+
     }
 }
